7d09f178ca4ba46372bd57d1e0d158c27fcf8882
ali <simsek.ali@metu.edu.tr>
2018-12-23 03:03:41 +0300

    signin & signup

--- image_annotation/urls.py
+++ image_annotation/urls.py
@@ -7,4 +7,7 @@ app_name = 'image_annotation'
 urlpatterns = [
     # /music/
     path('', views.index, name='index'),
+    path('sign_in/', views.signin, name='signin'),
+    path('sign_up/', views.signup, name='signup'),
+    path('pwd_confirmation/', views.confirmation, name='confirmation')
 ]
--- image_annotation/views.py
+++ image_annotation/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def index(request):
-    return render(request, 'image_annotation/sign_up.html', {"current_name" : "ali"})
+    return render(request, 'image_annotation/home_page.html', {"current_name" : "ali"})
+
+def signin(request):
+    return render(request, 'image_annotation/sign_in.html')
+
+def signup(request):
+    return render(request, 'image_annotation/sign_up.html')
+
+def confirmation(request):
+    if request.method == 'POST':
+        username = request.POST["Username"]
+        return HttpResponse(username)
+    return render(request, 'image_annotation/confirmation.html')
